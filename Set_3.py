3b83c08cc50e1ebb0b85676be58a3641b3d4fb4a
Krutik Dadhaniya <99273176+KD682@users.noreply.github.com>
2022-04-17 14:03:36 +0530

    Create Set_3.py

---
+++ Set_3.py
@@ -0,0 +1,7 @@
+s3 = {10,11,12,13,14,15}
+s4 = {16,17,18,19,20,21}
+
+print('Intersection : ', s3.intersection(s4))
+print('Union : ', s3.union(s4))
+print('Difference from s3 : ', s3.difference(s4))
+print('Difference from s4 : ', s4.difference(s3))
